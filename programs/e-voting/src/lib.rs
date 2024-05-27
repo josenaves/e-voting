d14f77c29b0ce26c5a10af66371d80533d2dbcb5
José Naves Moura Neto <josenaves@gmail.com>
2024-05-27 08:52:16 +0200

    Work in progress

--- programs/e-voting/src/lib.rs
+++ programs/e-voting/src/lib.rs
@@ -2,13 +2,14 @@ use anchor_lang::prelude::*;
 
 declare_id!("7UfykF9iXWorPS7A3SvgZmJzCTCxpVEqfLyBPw4K51YH");
 
-pub const TOPIC_LENGTH: usize = 32;
+pub const PROPOSAL_SEED : &str= "evoting_proposal_seed";
+pub const VOTE_SEED : &str= "evoting_vote_seed";
 
 #[program]
 pub mod e_voting {
     use super::*;
 
-    pub fn create(ctx: Context<Create>, description: String) -> Result<()> {
+    pub fn create(ctx: Context<CreateProposal>, description: String) -> Result<()> {
         require!(
             description.as_bytes().len() <= Proposal::DESCRIPTION_MAXIMUM_LENGTH, 
             EVotingError::DescriptionTooLong
@@ -20,18 +21,109 @@ pub mod e_voting {
         proposal.no_votes = 0;
         proposal.ongoing = true;
         proposal.owner = *ctx.accounts.user.key;
+
+        proposal.bump = ctx.bumps.proposal;
+
         Ok(())
+    }
+
+    pub fn vote_yes(ctx: Context<AddVote>, description: String) -> Result<()> {
+        vote(ctx, VoteType::YesVote, description)
+    }
+
+    pub fn vote_no(ctx: Context<AddVote>, description: String) -> Result<()> {
+        vote(ctx, VoteType::NoVote, description)
     }
 }
 
+fn vote(ctx: Context<AddVote>, vote_type: VoteType, description: String) -> Result<()> {
+    let proposal = &mut ctx.accounts.proposal;
+    let vote = &mut ctx.accounts.vote;
+    
+    msg!("proposal: {}", proposal.key());
+    msg!("vote: {}", vote.key());
+
+    //  proposal must be ongoing
+    require!(
+        proposal.ongoing == true,
+        EVotingError::VotingSessionIsClosed
+    );
+    
+    // vote.user = ctx.accounts.user.key();
+    vote.user = *ctx.accounts.user.key;
+    vote.proposal = proposal.key();
+    vote.bump = ctx.bumps.vote;
+    vote.description = description;
+
+    match vote_type {
+        VoteType::YesVote => {
+            proposal.yes_votes = proposal.yes_votes.checked_add(1).ok_or(EVotingError::MaxYesVotesReached)?;
+            vote.vote = VoteType::YesVote;
+        }
+        VoteType::NoVote => {
+            proposal.no_votes = proposal.no_votes.checked_add(1).ok_or(EVotingError::MaxNoVotesReached)?;
+            vote.vote = VoteType::NoVote;
+        }
+    }
+
+    Ok(())
+}
+
+// -----------------------
+// Instructions
+
 #[derive(Accounts)]
-pub struct Create<'info> {
-    #[account(init, payer=user, space=8 + Proposal::INIT_SPACE, seeds=[b"evoting", user.key().as_ref()], bump)]
+#[instruction(description: String)]
+pub struct CreateProposal<'info> {
+    #[account(
+        init, 
+        payer=user, 
+        space=8 + Proposal::INIT_SPACE, 
+        seeds=[
+            description.as_bytes(),
+            PROPOSAL_SEED.as_bytes(),
+            user.key().as_ref(),
+        ], 
+        bump
+    )]  
     pub proposal: Account<'info, Proposal>,
     #[account(mut)]
     pub user: Signer<'info>,
     pub system_program: Program<'info, System>,
 }
+
+#[derive(Accounts)]
+#[instruction(description: String)]
+pub struct AddVote<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(
+        init,
+        payer = user,
+        space = 8 + Vote::INIT_SPACE,
+        seeds = [
+            VOTE_SEED.as_bytes(), 
+            user.key().as_ref(),
+            proposal.key().as_ref(),
+        ], 
+        bump
+    )]
+    pub vote: Account<'info, Vote>,
+    #[account(
+        mut,
+        seeds = [
+            description.as_bytes(),
+            PROPOSAL_SEED.as_bytes(),
+            user.key().as_ref(),
+        ],
+        bump = proposal.bump
+    )]
+    pub proposal: Account<'info, Proposal>,
+    pub system_program: Program<'info, System>,
+}
+
+// ----------------------------
+// State
 
 #[account]
 #[derive(InitSpace)]
@@ -42,14 +134,40 @@ pub struct Proposal {
     pub no_votes: u32,
     pub ongoing: bool,
     pub owner: Pubkey,
+    pub bump: u8,
 }
 
 impl Proposal {
     pub const DESCRIPTION_MAXIMUM_LENGTH: usize = 50;
 }
 
+#[account]
+#[derive(InitSpace)]
+pub struct Vote {
+    pub user: Pubkey,
+    pub proposal: Pubkey,
+    pub vote: VoteType,
+    #[max_len(50)]
+    pub description: String,
+    pub bump: u8,
+}
+
+#[derive(AnchorDeserialize, AnchorSerialize, Clone, InitSpace)]
+pub enum VoteType {
+    YesVote,
+    NoVote
+}
+
 #[error_code]
 pub enum EVotingError {
     #[msg("Cannot initialize, description too long")]
-    DescriptionTooLong
+    DescriptionTooLong,
+    #[msg("Voting session is closed")]
+    VotingSessionIsClosed,
+    #[msg("You can just submit one vote per proposal")]
+    DuplicatedVoteNotAllowed,
+    #[msg("Maximum number of yes votes reached")]
+    MaxYesVotesReached,
+    #[msg("Maximum number of no votes reached")]
+    MaxNoVotesReached,
 }
--- tests/e-voting.ts
+++ tests/e-voting.ts
@@ -1,11 +1,14 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
-import { PublicKey } from '@solana/web3.js'
+import { PublicKey, SystemProgram } from '@solana/web3.js'
 import { EVoting } from "../target/types/e_voting";
 import { assert, expect } from "chai";
 
-const EVOTING_SEED = "evoting";
-const DESCRIPTION = "Proposal #1";
+const PROPOSAL_SEED = "evoting_proposal_seed";
+const VOTE_SEED = "evoting_vote_seed";
+
+const DESCRIPTION_1 = "Proposal #1";
+const DESCRIPTION_2 = "Proposal #2";
 
 describe("e-voting system", () => {
   // Configure the client to use the local cluster.
@@ -14,24 +17,38 @@ describe("e-voting system", () => {
 
   const program = anchor.workspace.EVoting as Program<EVoting>;
 
-  const [proposalPDA, _] = PublicKey.findProgramAddressSync(
-    [
-      anchor.utils.bytes.utf8.encode(EVOTING_SEED),
-      provider.wallet.publicKey.toBuffer(),
-    ],
-    program.programId
-  )
+  const admin = anchor.web3.Keypair.generate();
+  const joe = anchor.web3.Keypair.generate();
+  const juliana = anchor.web3.Keypair.generate();
+
+  console.log("program.programId", program.programId);
+  console.log("admin", admin.publicKey);
+  console.log("joe", joe.publicKey);
+  console.log("juliana", juliana.publicKey);
+
+  before("prepare", async () => {
+    await airdrop(anchor.getProvider().connection, admin.publicKey);
+    await airdrop(anchor.getProvider().connection, joe.publicKey);
+    await airdrop(anchor.getProvider().connection, juliana.publicKey);
+  })
 
   it("Proposal cannot have large description (greater than 50)", async () => {
+    let veryLargeDescription = "Very large description that will break the program!";
+
+    const [proposalPDA] = getProposalAddress(veryLargeDescription, admin.publicKey, program.programId);
+    console.log("proposalPDA", proposalPDA);
+
     let should_fail = "This Should Fail"
     try {
       await program.methods
-        .create("Very large description that will break the program!")
+        .create(veryLargeDescription)
         .accounts({
-          user: provider.wallet.publicKey,
+          user: admin.publicKey,
           proposal: proposalPDA,
+          systemProgram: anchor.web3.SystemProgram.programId
         })
-        .rpc();
+        .signers([admin])
+        .rpc({ commitment: "confirmed" });
     } catch (error) {
       const err = anchor.AnchorError.parse(error.logs);
       assert.strictEqual(err.error.errorCode.code, "DescriptionTooLong");
@@ -40,23 +57,140 @@ describe("e-voting system", () => {
     assert.strictEqual(should_fail, "Failed")
   });
 
-  it("Proposal creation", async () => {
+  it("Proposal creation #1", async () => {
+    const [proposalPDA] = getProposalAddress(DESCRIPTION_1, admin.publicKey, program.programId);
+    console.log("proposalPDA", proposalPDA);
+
     const tx = await program.methods
-      .create(DESCRIPTION)
+      .create(DESCRIPTION_1)
       .accounts({
-        user: provider.wallet.publicKey,
+        user: admin.publicKey,
         proposal: proposalPDA,
+        systemProgram: SystemProgram.programId
       })
-      .rpc();
+      .signers([admin])
+      .rpc({ commitment: "confirmed" });
 
     console.log("Your transaction signature", tx);
 
     let proposal = await program.account.proposal.fetch(proposalPDA);
     console.log("Proposal", proposal);
 
-    expect(proposal.description).to.equal(DESCRIPTION);
+    expect(proposal.description).to.equal(DESCRIPTION_1);
     expect(proposal.noVotes).to.equal(0);
     expect(proposal.yesVotes).to.equal(0);
     expect(proposal.ongoing).to.equal(true);
   });
+
+
+  it("Proposal creation #2", async () => {
+    const [proposalPDA] = getProposalAddress(DESCRIPTION_2, admin.publicKey, program.programId);
+    console.log("proposalPDA", proposalPDA);
+
+    const tx = await program.methods
+      .create(DESCRIPTION_2)
+      .accounts({
+        user: admin.publicKey,
+        proposal: proposalPDA,
+        systemProgram: SystemProgram.programId
+      })
+      .signers([admin])
+      .rpc({ commitment: "confirmed" });
+
+    console.log("Your transaction signature", tx);
+
+    let proposal = await program.account.proposal.fetch(proposalPDA);
+    console.log("Proposal", proposal);
+
+    expect(proposal.description).to.equal(DESCRIPTION_2);
+    expect(proposal.noVotes).to.equal(0);
+    expect(proposal.yesVotes).to.equal(0);
+    expect(proposal.ongoing).to.equal(true);
+  });
+
+  it("Joe is voting yes", async () => {
+    let [proposalPDA] = getProposalAddress(DESCRIPTION_1, admin.publicKey, program.programId);
+    console.log("proposalPDA", proposalPDA);
+
+    let [votePDA] = getVoteAddress(joe.publicKey, proposalPDA, program.programId);
+    console.log("votePDA", votePDA);
+
+    console.log("joe.publicKey", joe.publicKey);
+    console.log("SystemProgram.programId", SystemProgram.programId);
+
+    const tx = await program.methods
+      .voteYes(DESCRIPTION_1)
+      .accounts({
+        user: joe.publicKey,
+        proposal: proposalPDA,
+        vote: votePDA,
+        systemProgram: SystemProgram.programId,
+      })
+      .signers([joe])
+      // .rpc({ commitment: "confirmed" });
+      .rpc({ skipPreflight: true});
+
+    console.log("Your transaction signature", tx);
+
+    // let proposal = await program.account.proposal.fetch(proposalPDA);
+    // console.log("Proposal", proposal);
+
+    // let vote = await program.account.vote.fetch(votePDA);
+    // console.log("Vote", vote);
+  });
+
+//   it("Juliana is voting no", async () => {
+//     let [proposalPDA] = getProposalAddress(admin.publicKey, program.programId);
+//     console.log("proposalPDA", proposalPDA);
+
+//     let [votePDA] = getVoteAddress(juliana.publicKey, proposalPDA, program.programId);
+//     console.log("votePDA", votePDA);
+// 4
+//     const tx = await program.methods
+//       .voteNo()
+//       .accounts({
+//         user: juliana.publicKey,
+//         proposal: proposalPDA,
+//         vote: votePDA,
+//         systemProgram: SystemProgram.programId
+//       })
+//       .signers([juliana])
+//       // .rpc({ commitment: "confirmed" });
+//       .rpc({ skipPreflight: true});
+
+//     console.log("Your transaction signature", tx);
+
+//     let proposal = await program.account.proposal.fetch(proposalPDA);
+//     console.log("Proposal", proposal);
+
+//     let vote = await program.account.vote.fetch(votePDA);
+//     console.log("Vote", vote);
+//   });
+
 });
+
+function getProposalAddress(description: String, author: PublicKey, programID: PublicKey) {
+  return PublicKey.findProgramAddressSync(
+    [
+      Buffer.from(description),
+      Buffer.from(PROPOSAL_SEED),
+      author.toBuffer()
+    ], 
+    programID
+  );
+}
+
+function getVoteAddress(author: PublicKey, proposal: PublicKey, programID: PublicKey) {
+  return PublicKey.findProgramAddressSync(
+    [
+      Buffer.from(VOTE_SEED),
+      author.toBuffer(),
+      proposal.toBuffer(),
+    ], 
+    programID
+  );
+}
+
+async function airdrop(connection: any, address: any, amount = 1000000000) {
+  await connection.confirmTransaction(await connection.requestAirdrop(address, amount), "confirmed");
+}
